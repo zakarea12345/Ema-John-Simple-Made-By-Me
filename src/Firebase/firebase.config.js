b1418d6bbb36a270b77ca597b10a6d53cdef1b3f
zakarea12345 <saiakzakarea7@gmail.com>
2021-10-09 22:25:43 +0600

    firebase intallattion done

---
+++ src/Firebase/firebase.config.js
@@ -0,0 +1,10 @@
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyDusFx4cFxVLANLc6RYQl5uxs4atbCUyHA",
+    authDomain: "simple-ema-john-react-project.firebaseapp.com",
+    projectId: "simple-ema-john-react-project",
+    storageBucket: "simple-ema-john-react-project.appspot.com",
+    messagingSenderId: "997540726786",
+    appId: "1:997540726786:web:ec8566737fc86ac8b7ff1b"
+  };
+  export default firebaseConfig;
